4187488a592711eaf915c5a416a0b517cf2728af
WLONEGI <yuki.negishi.wlo@gmail.com>
2025-02-24 12:17:21 +0900

    first-commit

--- src/icp-test-backend/src/lib.rs
+++ src/icp-test-backend/src/lib.rs
@@ -1,4 +1,23 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use std::cell::RefCell;
+
+pub struct Data {
+    message: String,
+}
+
+thread_local! {
+    static DATA: RefCell<Data> = RefCell::new(Data {message: String::new()});
+}
+
+#[ic_cdk::query(name="getMessage")]
+fn get_message() -> String {
+    DATA.with(|data| {
+      data.borrow().message.clone()
+    })
+}
+
+#[ic_cdk::update(name="setMessage")]
+fn set_message(message: String) -> () {
+    DATA.with(|data| {
+      data.borrow_mut().message = message;
+    });
 }
